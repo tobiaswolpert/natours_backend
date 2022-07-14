f7209c456f3c6581b77feda3ace6b797638b6026
Wolpert Tobias GCR EOQMM4 <tobias.wolpert@gmail.com>
2022-07-14 16:09:45 -0400

    Debugging stripe

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -34,6 +34,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 });
 
 const createBookingCheckout = async (session) => {
+  console.log("SESSION", session);
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = (await session.line_items[0].amount) / 100;
@@ -42,6 +43,7 @@ const createBookingCheckout = async (session) => {
 };
 
 exports.webhookCheckout = (req, res, next) => {
+  console.log("SIGNATURE", signature);
   const signature = req.headers["stripe-signature"];
   let event;
 
